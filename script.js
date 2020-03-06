dd64561a50c044e531b10a00750fec02ade50b64
Leonardo Oliveira <leozartino@gmail.com>
2020-03-06 11:20:27 -0300

    feat: add func que verificam o camp, emapte e etc

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const X_CLASS = "x";
 const CIRCULO_CLASS = "circulo";
+
 const combinacoes_vencedores = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,24 +13,55 @@ const combinacoes_vencedores = [
 ];
 
 const elementosQuadrados = document.querySelectorAll("[data-quadrado]");
-
+const msgElemento = document.getElementById("msgCampeao");
+const msgVencedor = document.querySelector("[data-texto-vencedor]");
+const butaoRestart = document.getElementById("jogarNovamente");
 let turnoCirculo;
 
-(function startGame() {
+startGame();
+
+butaoRestart.addEventListener("click", startGame);
+
+function startGame() {
   turnoCirculo = false;
   elementosQuadrados.forEach(quadrado => {
+    quadrado.classList.remove(X_CLASS);
+    quadrado.classList.remove(CIRCULO_CLASS);
+    quadrado.removeEventListener("click", handleClick);
     quadrado.addEventListener("click", handleClick, { once: true });
   });
   setBoardHoverClass();
-})();
+  msgElemento.classList.remove("show");
+}
+
+function fimGame(empate) {
+  empate
+    ? (msgVencedor.innerText = "Empate!")
+    : (msgVencedor.innerText = `${turnoCirculo ? "O" : "X"} Venceu!`);
+  msgElemento.classList.add("show");
+}
 
 function handleClick(e) {
   const quadrado = e.target;
   const classeAtual = turnoCirculo ? CIRCULO_CLASS : X_CLASS;
   porMarcador(quadrado, classeAtual);
-  //Verificar o vencedor
-  trocaTurnos();
-  setBoardHoverClass();
+  if (verifcaVencedor(classeAtual)) {
+    fimGame(false);
+  } else if (foiEmpate()) {
+    fimGame(true);
+  } else {
+    trocaTurnos();
+    setBoardHoverClass();
+  }
+}
+
+function foiEmpate() {
+  return [...elementosQuadrados].every(quadrado => {
+    return (
+      quadrado.classList.contains(X_CLASS) ||
+      quadrado.classList.contains(CIRCULO_CLASS)
+    );
+  });
 }
 
 function porMarcador(quadrado, classeAtual) {
@@ -47,4 +79,12 @@ function setBoardHoverClass() {
   turnoCirculo
     ? board.classList.add(CIRCULO_CLASS)
     : board.classList.add(X_CLASS);
+}
+
+function verifcaVencedor(classeAtual) {
+  return combinacoes_vencedores.some(combinacao => {
+    return combinacao.every(index => {
+      return elementosQuadrados[index].classList.contains(classeAtual);
+    });
+  });
 }
